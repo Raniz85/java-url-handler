6f3dcbe76dc4287e0407e4c58075f2d92c84d653
Raniz <raniz@raneland.se>
2015-08-25 22:02:25 +0200

    Implemented header fields for S3UrlConnection

--- java-url-handler-aws-s3/src/main/java/se/raneland/urlhandler/aws/s3/S3UrlConnection.java
+++ java-url-handler-aws-s3/src/main/java/se/raneland/urlhandler/aws/s3/S3UrlConnection.java
@@ -13,12 +13,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 /**
  * {@link URLConnection} implementation for Amazon S3 urls.
+ *
+ * Fetches the {@link ObjectMetadata} when connecting and the {@link S3Object}
+ * when calling {@link URLConnection#getInputStream()}.
+ *
  * @author Raniz
+ * @since 1.0
  */
 public class S3UrlConnection extends URLConnection {
+
+    // Format dates according to the HTTP spec
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.RFC_1123_DATE_TIME;
 
     private final AwsClientFactory<? extends AmazonS3> clientFactory;
     private final String bucketName;
@@ -41,6 +51,11 @@ public class S3UrlConnection extends URLConnection {
         this.metadata = s3.getObjectMetadata(bucketName, keyName);
     }
 
+    /**
+     * Create the {@link ClientOptions} that should be use to retrieve the {@link AmazonS3} client from the {@link AwsClientFactory}.
+     *
+     * @return
+     */
     protected ClientOptions createClientOptions() {
         ClientOptions options = new ClientOptions();
         String userInfo = url.getUserInfo();
@@ -69,7 +84,95 @@ public class S3UrlConnection extends URLConnection {
         return options;
     }
 
-    // TODO: Implement header fields
+    @Override
+    public long getContentLengthLong() {
+        return metadata.getContentLength();
+    }
+
+    @Override
+    public String getContentType() {
+        return metadata.getContentType();
+    }
+
+    @Override
+    public String getContentEncoding() {
+        return metadata.getContentEncoding();
+    }
+
+    @Override
+    public long getExpiration() {
+        Date expires = metadata.getHttpExpiresDate();
+        if(expires != null) {
+            return expires.toInstant().toEpochMilli();
+        }
+        return 0;
+    }
+
+    @Override
+    public long getLastModified() {
+        Date lastModified = metadata.getLastModified();
+        if(lastModified != null) {
+            return lastModified.toInstant().toEpochMilli();
+        }
+        return 0;
+    }
+
+    @Override
+    public String getHeaderField(String name) {
+        name = name.toLowerCase();
+        switch(name) {
+            case "cache-control":
+                return metadata.getCacheControl();
+            case "content-disposition":
+                return metadata.getContentDisposition();
+            case "content-encoding":
+                return metadata.getContentEncoding();
+            case "content-length":
+                return String.valueOf(metadata.getContentLength());
+            case "content-md5":
+                return metadata.getContentMD5();
+            case "content-type":
+                return metadata.getContentType();
+            case "etag":
+                return metadata.getETag();
+            case "last-modified":
+                Date lastModified = metadata.getLastModified();
+                if(lastModified != null) {
+                    return DATE_TIME_FORMATTER.format(lastModified.toInstant());
+                }
+                return null;
+            case "expires":
+                Date expires = metadata.getHttpExpiresDate();
+                if(expires != null) {
+                    return DATE_TIME_FORMATTER.format(expires.toInstant());
+                }
+                return null;
+        }
+        return metadata.getUserMetaDataOf(name);
+    }
+
+    @Override
+    public Map<String, List<String>> getHeaderFields() {
+        Map<String, List<String>> headers = new HashMap<>();
+
+        // Add custom headers
+        for(Map.Entry<String, String> entry : metadata.getUserMetadata().entrySet()) {
+            headers.put(entry.getKey(), Collections.singletonList(entry.getValue()));
+        }
+
+        // Add standard headers
+        headers.put("Cache-Control", Collections.singletonList(metadata.getCacheControl()));
+        headers.put("Content-Disposition", Collections.singletonList(metadata.getContentDisposition()));
+        headers.put("Content-Encoding", Collections.singletonList(metadata.getContentEncoding()));
+        headers.put("Content-Length", Collections.singletonList(String.valueOf(metadata.getContentLength())));
+        headers.put("Content-MD5", Collections.singletonList(metadata.getContentMD5()));
+        headers.put("Content-Type", Collections.singletonList(metadata.getContentType()));
+        headers.put("ETag", Collections.singletonList(metadata.getETag()));
+        headers.put("Last-Modified", Collections.singletonList(DATE_TIME_FORMATTER.format(metadata.getLastModified().toInstant())));
+        headers.put("Expires", Collections.singletonList(DATE_TIME_FORMATTER.format(metadata.getHttpExpiresDate().toInstant())));
+
+        return headers;
+    }
 
     @Override
     public InputStream getInputStream() throws IOException {
